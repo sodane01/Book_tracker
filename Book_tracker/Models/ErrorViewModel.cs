e00b5f07c4903ef7dff8fdf2bca44d2300131b5c
Anette Lindgren Söderström <sodane01@gmail.com>
2026-08-17 13:37:12 +0200

    Initial ASP.NET Core MVC project with Identity

---
+++ Book_tracker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Book_tracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

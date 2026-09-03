9d2203731cb663f589d310aeb36fbc259944fbcb
Anette Lindgren Söderström <sodane01@gmail.com>
2026-09-03 14:09:08 +0200

    Finalize Book Tracker UI and home dashboard

--- Book_tracker/Controllers/HomeController.cs
+++ Book_tracker/Controllers/HomeController.cs
@@ -1,19 +1,69 @@
 using Book_tracker.Models;
+using Book_tracker.Models.Enums;
+using Book_tracker.Services;
+using Book_tracker.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-using Book_tracker.ViewModels;
 
 namespace Book_tracker.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IUserBookService _userBookService;
+
+        public HomeController(
+            UserManager<ApplicationUser> userManager,
+            IUserBookService userBookService)
         {
+            _userManager = userManager;
+            _userBookService = userBookService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var isAuthenticated =
+                User.Identity?.IsAuthenticated ?? false;
+
             var viewModel = new HomeViewModel
             {
-                IsAuthenticated = User.Identity?.IsAuthenticated ?? false,
+                IsAuthenticated = isAuthenticated,
                 UserName = User.Identity?.Name
             };
+
+            if (!isAuthenticated)
+            {
+                return View(viewModel);
+            }
+
+            var user =
+                await _userManager.GetUserAsync(User);
+
+            if (user == null)
+            {
+                return View(viewModel);
+            }
+
+            var userBooks =
+                await _userBookService.GetUserBooksAsync(user.Id);
+
+            viewModel.CurrentlyReadingCount =
+                userBooks.Count(userBook =>
+                    userBook.ReadingStatus ==
+                    ReadingStatus.CurrentlyReading);
+
+            viewModel.BooksReadCount =
+                userBooks.Count(userBook =>
+                    userBook.ReadingStatus ==
+                    ReadingStatus.Read);
+
+            viewModel.FavouriteBooksCount =
+                userBooks.Count(userBook =>
+                    userBook.IsFavourite);
+
+            // Notifications are not connected to the Home statistics yet.
+            viewModel.UnreadNotificationCount = 0;
 
             return View(viewModel);
         }
@@ -23,10 +73,19 @@ namespace Book_tracker.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [ResponseCache(
+            Duration = 0,
+            Location = ResponseCacheLocation.None,
+            NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(
+                new ErrorViewModel
+                {
+                    RequestId =
+                        Activity.Current?.Id ??
+                        HttpContext.TraceIdentifier
+                });
         }
     }
 }
